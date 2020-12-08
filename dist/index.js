bb90e39e72e5e4340dd55ccb530e607f129e3a82
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2020-12-08 17:24:00 -0300

    Correção no nome das variáveis de cache

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -22,14 +13,14 @@ exports.CacheLevel = cacheLevel_1.default;
 const debug = debug_1.default('api:cache');
 class Cache {
     static create(app, level, userCache) {
-        debug(`Criando cache ${app.info.name}-${level}`);
+        debug(`Creating cache ${app.info.name}-${level}`);
         const cacheoptions = {
             namespace: `${app.info.name}${level}`,
             defaultTtl: app.config.cache.ttl[level],
             sessionAware: true,
             genCacheKey: (req, res) => {
-                const system = req.sistema ? req.sistema.identificacao : undefined;
-                const userId = req.usuario ? req.usuario.id : 'unknown';
+                const system = req.user ? req.user.identificacao : undefined;
+                const userId = req.system ? req.system.id : 'unknown';
                 const method = req.method;
                 const resource = req.originalUrl;
                 if (userCache) {
@@ -40,7 +31,7 @@ class Cache {
             engine: expeditious_engine_redis_1.default(app.config)
         };
         const instance = express_expeditious_1.default(cacheoptions);
-        debug(`Armazenando cache ${app.info.name}-${level}`);
+        debug(`Storing cache ${app.info.name}-${level}`);
         Cache.caches.push({
             name: userCache ? `${app.info.name}-${level}-p` : `${app.info.name}-${level}`,
             level,
@@ -48,18 +39,16 @@ class Cache {
         });
         return instance;
     }
-    static flush(level, callback) {
-        return __awaiter(this, void 0, void 0, function* () {
-            debug(`Invalidando caches de nível ${level}`);
-            for (const cache of Cache.caches) {
-                if (cache.level === level) {
-                    cache.instance.flush(cache.name, callback ? callback : () => {
-                        //
-                    });
-                }
+    static async flush(level, callback) {
+        debug(`Invalidating level ${level} cache`);
+        for (const cache of Cache.caches) {
+            if (cache.level === level) {
+                cache.instance.flush(cache.name, callback ? callback : () => {
+                    //
+                });
             }
-            return Promise.resolve();
-        });
+        }
+        return Promise.resolve();
     }
 }
 Cache.caches = [];
